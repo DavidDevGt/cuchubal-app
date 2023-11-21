26e788da1c339b942a9f4903cb5210ed1720aa83
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-21 10:01:51 -0600

    Refactor Participant and ParticipantService classes

--- backend/models/Participant.php
+++ backend/models/Participant.php
@@ -1,10 +1,53 @@
 <?php
-class Participant {
+require_once('../db/database.php');
+
+class Participant
+{
     public $id;
     public $name;
     public $contact;
     public $address;
     public $paymentMethod;
 
-    // Constructor, getters, setters, etc.
+    // Constructor
+    public function __construct($id, $name, $contact, $address, $paymentMethod)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->contact = $contact;
+        $this->address = $address;
+        $this->paymentMethod = $paymentMethod;
+    }
+
+    // Getters y setters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function getPaymentMethod()
+    {
+        return $this->paymentMethod;
+    }
+
+    // Método para verificar si el participante está activo
+    public function isActive()
+    {
+        // Lógica para verificar si el participante está activo
+    }
 }
--- backend/services/participantService.php
+++ backend/services/participantService.php
@@ -1,22 +1,45 @@
 <?php
-class ParticipantService {
+require_once('../models/Participant.php');
+
+class ParticipantService
+{
     // Crear un nuevo participante
-    public function createParticipant($data) {
-        // SQL para insertar un nuevo participante
+    public function createParticipant($name, $contact, $address, $paymentMethod)
+    {
+        $sql = "INSERT INTO participants (name, contact, address, payment_method) VALUES (?, ?, ?, ?)";
+        return dbQueryPreparada($sql, [$name, $contact, $address, $paymentMethod]);
     }
 
     // Leer participantes (activos)
-    public function listActiveParticipants() {
-        // SQL para seleccionar participantes donde active = 1
+    public function listActiveParticipants()
+    {
+        $sql = "SELECT * FROM participants WHERE active = 1";
+        $result = dbQuery($sql);
+        $participants = [];
+        while ($row = dbFetchAssoc($result)) {
+            $participants[] = $row;
+        }
+        return $participants;
     }
 
     // Actualizar un participante
-    public function updateParticipant($id, $data) {
-        // SQL para actualizar un participante por id
+    public function updateParticipant($id, $name, $contact, $address, $paymentMethod)
+    {
+        $sql = "UPDATE participants SET name = ?, contact = ?, address = ?, payment_method = ? WHERE id = ?";
+        dbQueryPreparada($sql, [$name, $contact, $address, $paymentMethod, $id]);
     }
 
     // Soft delete de un participante
-    public function softDeleteParticipant($id) {
-        // SQL para actualizar el campo 'active' a 0
+    public function softDeleteParticipant($id)
+    {
+        $sql = "UPDATE participants SET active = 0 WHERE id = ?";
+        dbQueryPreparada($sql, [$id]);
+    }
+
+    // Método para obtener un participante por su nombre o contacto
+    public function getParticipantByNameOrContact($searchTerm)
+    {
+        $sql = "SELECT * FROM participants WHERE (name LIKE ? OR contact LIKE ?) AND active = 1";
+        return dbQueryPreparada($sql, ['%' . $searchTerm . '%', '%' . $searchTerm . '%']);
     }
 }
