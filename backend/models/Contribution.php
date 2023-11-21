fb41d6872d637e3ae218277bf3b5deb324de8cb6
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-21 13:06:35 -0600

    Refactor Contribution class and ContributionService

--- backend/models/Contribution.php
+++ backend/models/Contribution.php
@@ -1,9 +1,34 @@
 <?php
+require_once('../db/database.php');
+
 class Contribution {
     public $id;
     public $participantId;
     public $amount;
     public $date;
 
-    // Constructor, getters, setters, etc.
+    // Constructor
+    public function __construct($id, $participantId, $amount, $date) {
+        $this->id = $id;
+        $this->participantId = $participantId;
+        $this->amount = $amount;
+        $this->date = $date;
+    }
+
+    // Getters y setters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getParticipantId() {
+        return $this->participantId;
+    }
+
+    public function getAmount() {
+        return $this->amount;
+    }
+
+    public function getDate() {
+        return $this->date;
+    }
 }
--- backend/services/contributionService.php
+++ backend/services/contributionService.php
@@ -1,22 +1,49 @@
 <?php
-class ContributionService {
+require_once('../models/Contribution.php');
+
+class ContributionService
+{
     // Crear una nueva contribución
-    public function addContribution($data) {
-        // SQL para insertar una nueva contribución
+    public function addContribution($participantId, $amount, $date)
+    {
+        $sql = "INSERT INTO contributions (participant_id, amount, date) VALUES (?, ?, ?)";
+        return dbQueryPreparada($sql, [$participantId, $amount, $date]);
     }
 
     // Leer contribuciones (activas)
-    public function listActiveContributions() {
-        // SQL para seleccionar contribuciones donde active = 1
+    public function listActiveContributions()
+    {
+        $sql = "SELECT * FROM contributions WHERE active = 1";
+        $result = dbQuery($sql);
+        $contributions = [];
+        while ($row = dbFetchAssoc($result)) {
+            $contributions[] = $row;
+        }
+        return $contributions;
     }
 
     // Actualizar una contribución
-    public function updateContribution($id, $data) {
-        // SQL para actualizar una contribución por id
+    public function updateContribution($id, $participantId, $amount, $date)
+    {
+        $sql = "UPDATE contributions SET participant_id = ?, amount = ?, date = ? WHERE id = ?";
+        return dbQueryPreparada($sql, [$participantId, $amount, $date, $id]);
     }
 
     // Soft delete de una contribución
-    public function softDeleteContribution($id) {
-        // SQL para actualizar el campo 'active' a 0
+    public function softDeleteContribution($id)
+    {
+        $sql = "UPDATE contributions SET active = 0 WHERE id = ?";
+        return dbQueryPreparada($sql, [$id]);
+    }
+
+    // Total de contribuciones
+    public function getTotalContributions()
+    {
+        $sql = "SELECT SUM(amount) AS total FROM contributions WHERE active = 1";
+        $result = dbQuery($sql);
+        if ($row = dbFetchAssoc($result)) {
+            return $row['total'];
+        }
+        return 0;
     }
 }
