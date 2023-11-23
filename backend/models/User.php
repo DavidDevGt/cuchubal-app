18ce4fae287398321f0ab83d0d4c5ae5ea907e0e
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-23 09:24:51 -0600

    Add User model and userService class

---
+++ backend/models/User.php
@@ -0,0 +1,28 @@
+<?php
+require_once('../db/database.php');
+
+class User {
+    public $id;
+    public $username;
+    public $password;
+
+    // Constructor
+    public function __construct($id, $username, $password) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    // Getters y setters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+}
---
+++ backend/services/userService.php
@@ -0,0 +1,44 @@
+<?php
+require_once('../models/User.php');
+
+class userService {
+    // Crear un nuevo usuario
+    public function createUser($username, $password) {
+        $hash_password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        return dbQueryPreparada($sql, [$username, $hash_password]);
+    }
+
+    // Obtener un usuario por su username
+    public function getUserByUsername($username) {
+        $sql = "SELECT * FROM users WHERE username = ?";
+        $result = dbQueryPreparada($sql, [$username]);
+        if ($row = dbFetchAssoc($result)) {
+            return new User($row['id'], $row['username'], $row['password']);
+        }
+        return null;
+    }
+
+    // Actualizar la contraseña de un usuario
+    public function updateUserPassword($id, $newPassword) {
+        $sql = "UPDATE users SET password = ? WHERE id = ?";
+        return dbQueryPreparada($sql, [$newPassword, $id]);
+    }
+
+    // Obtener todos los usuarios
+    public function getAllUsers() {
+        $sql = "SELECT * FROM users";
+        $result = dbQuery($sql);
+        $users = [];
+        while ($row = dbFetchAssoc($result)) {
+            $users[] = new User($row['id'], $row['username'], $row['password']);
+        }
+        return $users;
+    }
+
+    // Soft delete de un usuario
+    public function softDeleteUser($id) {
+        $sql = "UPDATE users SET active = 0 WHERE id = ?";
+        return dbQueryPreparada($sql, [$id]);
+    }
+}
