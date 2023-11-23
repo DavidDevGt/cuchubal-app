acfd3ed28973148dd088b0737e23cc3b71357ccf
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-23 15:15:07 -0600

    Fix formatting and add new tables and columns

--- backend/db/cuchubal.sql
+++ backend/db/cuchubal.sql
@@ -18,24 +18,24 @@ CREATE TABLE
     contributions (
         id INT AUTO_INCREMENT PRIMARY KEY,
         participant_id INT,
-        amount DECIMAL(10,2),
+        amount DECIMAL(10, 2),
         date DATE,
         active TINYINT (1) DEFAULT 1,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (participant_id) REFERENCES participants(id)
+        FOREIGN KEY (participant_id) REFERENCES participants (id)
     );
 
 CREATE TABLE
     payments (
         id INT AUTO_INCREMENT PRIMARY KEY,
         participant_id INT,
-        amount DECIMAL(10,2),
+        amount DECIMAL(10, 2),
         date DATE,
         active TINYINT (1) DEFAULT 1,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (participant_id) REFERENCES participants(id)
+        FOREIGN KEY (participant_id) REFERENCES participants (id)
     );
 
 CREATE TABLE
@@ -46,4 +46,40 @@ CREATE TABLE
         active TINYINT (1) DEFAULT 1,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE
+    cuchubales (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT,
+        name VARCHAR(255) NOT NULL,
+        description TEXT,
+        active TINYINT (1) DEFAULT 1,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users (id)
+    );
+
+ALTER TABLE participants
+ADD COLUMN cuchubal_id INT,
+ADD FOREIGN KEY (cuchubal_id) REFERENCES cuchubales (id);
+
+ALTER TABLE contributions
+ADD COLUMN cuchubal_id INT,
+ADD FOREIGN KEY (cuchubal_id) REFERENCES cuchubales (id);
+
+ALTER TABLE payments
+ADD COLUMN cuchubal_id INT,
+ADD FOREIGN KEY (cuchubal_id) REFERENCES cuchubales (id);
+
+CREATE TABLE
+    payment_schedule (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        cuchubal_id INT,
+        participant_id INT,
+        scheduled_date DATE,
+        amount DECIMAL(10, 2),
+        status VARCHAR(50), -- Por ejemplo: 'Pendiente', 'Completado'
+        FOREIGN KEY (cuchubal_id) REFERENCES cuchubales (id),
+        FOREIGN KEY (participant_id) REFERENCES participants (id)
     );
