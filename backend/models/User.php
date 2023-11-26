7d8a3a57e6ae998426ef2d317a8c1544ffa50ca6
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-26 15:56:51 -0600

    Refactor User class and UserService

--- backend/models/User.php
+++ backend/models/User.php
@@ -6,8 +6,7 @@ class User {
     public $username;
     public $password;
 
-    // Constructor
-    public function __construct($id, $username, $password) {
+    public function __construct($id = 0, $username = '', $password = '') {
         $this->id = $id;
         $this->username = $username;
         $this->password = $password;
@@ -24,5 +23,49 @@ class User {
 
     public function getPassword() {
         return $this->password;
+    }
+
+    // Setters
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    public function setPassword($password) {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    // Métodos de Base de Datos
+    public function save() {
+        if ($this->id == 0) {
+            $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+            $this->id = dbQueryPreparada($sql, [$this->username, $this->password]);
+        } else {
+            $sql = "UPDATE users SET username = ?, password = ? WHERE id = ?";
+            dbQueryPreparada($sql, [$this->username, $this->password, $this->id]);
+        }
+    }
+
+    public static function getById($id) {
+        $sql = "SELECT * FROM users WHERE id = ?";
+        $result = dbQueryPreparada($sql, [$id]);
+        if ($row = dbFetchAssoc($result)) {
+            return new User($row['id'], $row['username'], $row['password']);
+        }
+        return null;
+    }
+
+    public static function getAll() {
+        $sql = "SELECT * FROM users";
+        $result = dbQuery($sql);
+        $users = [];
+        while ($row = dbFetchAssoc($result)) {
+            $users[] = new User($row['id'], $row['username'], $row['password']);
+        }
+        return $users;
+    }
+
+    public function delete() {
+        $sql = "UPDATE users SET active = 0 WHERE id = ?";
+        dbQueryPreparada($sql, [$this->id]);
     }
 }
--- backend/services/userService.php
+++ backend/services/userService.php
@@ -1,44 +1,36 @@
 <?php
 require_once __DIR__ . '/../models/User.php';
 
-class userService {
-    // Crear un nuevo usuario
+class UserService {
     public function createUser($username, $password) {
-        $hash_password = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-        return dbQueryPreparada($sql, [$username, $hash_password]);
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($password);
+        $user->save();
+        return $user->getId();
     }
 
-    // Obtener un usuario por su username
-    public function getUserByUsername($username) {
-        $sql = "SELECT * FROM users WHERE username = ?";
-        $result = dbQueryPreparada($sql, [$username]);
-        if ($row = dbFetchAssoc($result)) {
-            return new User($row['id'], $row['username'], $row['password']);
-        }
-        return null;
+    public function getUserById($id) {
+        return User::getById($id);
     }
 
-    // Actualizar la contraseña de un usuario
-    public function updateUserPassword($id, $newPassword) {
-        $sql = "UPDATE users SET password = ? WHERE id = ?";
-        return dbQueryPreparada($sql, [$newPassword, $id]);
-    }
-
-    // Obtener todos los usuarios
     public function getAllUsers() {
-        $sql = "SELECT * FROM users";
-        $result = dbQuery($sql);
-        $users = [];
-        while ($row = dbFetchAssoc($result)) {
-            $users[] = new User($row['id'], $row['username'], $row['password']);
-        }
-        return $users;
+        return User::getAll();
     }
 
-    // Soft delete de un usuario
-    public function softDeleteUser($id) {
-        $sql = "UPDATE users SET active = 0 WHERE id = ?";
-        return dbQueryPreparada($sql, [$id]);
+    public function updateUser($id, $username, $password) {
+        $user = User::getById($id);
+        if ($user) {
+            $user->setUsername($username);
+            $user->setPassword($password);
+            $user->save();
+        }
+    }
+
+    public function deleteUser($id) {
+        $user = User::getById($id);
+        if ($user) {
+            $user->delete();
+        }
     }
 }
