02b849fdfd15b969c3b5484bf44c36d9b54c382c
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-26 15:56:56 -0600

    Refactor Contribution and ContributionService classes

--- backend/models/Contribution.php
+++ backend/models/Contribution.php
@@ -1,34 +1,109 @@
 <?php
 require_once __DIR__ . '/../db/database.php';
 
-class Contribution {
+class Contribution
+{
     public $id;
     public $participantId;
     public $amount;
     public $date;
+    public $status;
+    public $cuchubalId;
 
     // Constructor
-    public function __construct($id, $participantId, $amount, $date) {
+    public function __construct($id = 0, $participantId = 0, $amount = 0.0, $date = '', $status = 'No pagado', $cuchubalId = 0)
+    {
         $this->id = $id;
         $this->participantId = $participantId;
         $this->amount = $amount;
         $this->date = $date;
+        $this->status = $status;
+        $this->cuchubalId = $cuchubalId;
     }
 
     // Getters y setters
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getParticipantId() {
+    public function getParticipantId()
+    {
         return $this->participantId;
     }
 
-    public function getAmount() {
+    public function getAmount()
+    {
         return $this->amount;
     }
 
-    public function getDate() {
+    public function getDate()
+    {
         return $this->date;
+    }
+
+    public function setId($value)
+    {
+        $this->id = $value;
+    }
+
+    public function setParticipantId($value)
+    {
+        $this->participantId = $value;
+    }
+
+    public function setAmount($value)
+    {
+        $this->amount = $value;
+    }
+
+    public function setDate($value)
+    {
+        $this->date = $value;
+    }
+
+    public function setCuchubalId($value)
+    {
+        $this->cuchubalId = $value;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($value)
+    {
+        $this->status = $value;
+    }
+
+    // Métodos
+    public function save()
+    {
+        if ($this->id == 0) {
+            // Crear nueva contribución
+            $sql = "INSERT INTO contributions (participant_id, amount, date, status, cuchubal_id) VALUES (?, ?, ?, ?, ?)";
+            $this->id = dbQueryPreparada($sql, [$this->participantId, $this->amount, $this->date, $this->status, $this->cuchubalId]);
+        } else {
+            // Actualizar contribución existente
+            $sql = "UPDATE contributions SET participant_id = ?, amount = ?, date = ?, status = ?, cuchubal_id = ? WHERE id = ?";
+            dbQueryPreparada($sql, [$this->participantId, $this->amount, $this->date, $this->status, $this->cuchubalId, $this->id]);
+        }
+    }
+
+    public static function getById($id)
+    {
+        $sql = "SELECT * FROM contributions WHERE id = ?";
+        $result = dbQueryPreparada($sql, [$id]);
+        if ($row = dbFetchAssoc($result)) {
+            return new Contribution($row['id'], $row['participant_id'], $row['amount'], $row['date'], $row['cuchubal_id']);
+        }
+        return null;
+    }
+
+    public function delete()
+    {
+        $sql = "DELETE FROM contributions WHERE id = ?";
+        dbQueryPreparada($sql, [$this->id]);
     }
 }
--- backend/services/contributionService.php
+++ backend/services/contributionService.php
@@ -3,44 +3,85 @@ require_once __DIR__ . '/../models/Contribution.php';
 
 class ContributionService
 {
+
     // Crear una nueva contribución
-    public function addContribution($participantId, $amount, $date)
+    public function createContribution($participantId, $amount, $date, $cuchubalId)
     {
-        $sql = "INSERT INTO contributions (participant_id, amount, date) VALUES (?, ?, ?)";
-        return dbQueryPreparada($sql, [$participantId, $amount, $date]);
+        $contribution = new Contribution(0, $participantId, $amount, $date, $cuchubalId);
+        $contribution->save();
+        return $contribution->getId();
     }
 
-    // Leer contribuciones (activas)
-    public function listActiveContributions()
+    // Obtener una contribución por ID
+    public function getContributionById($id)
     {
-        $sql = "SELECT * FROM contributions WHERE active = 1";
-        $result = dbQuery($sql);
+        return Contribution::getById($id);
+    }
+
+    // Actualizar una contribución existente
+    public function updateContribution($id, $participantId, $amount, $date, $cuchubalId)
+    {
+        $contribution = new Contribution($id, $participantId, $amount, $date, $cuchubalId);
+        $contribution->save();
+    }
+
+    // Eliminar una contribución
+    public function deleteContribution($id)
+    {
+        $contribution = Contribution::getById($id);
+        if ($contribution) {
+            $contribution->delete();
+        }
+    }
+
+    // Listar todas las contribuciones de un cuchubal específico
+    public function listContributionsByCuchubal($cuchubalId)
+    {
+        $sql = "SELECT * FROM contributions WHERE cuchubal_id = ?";
+        $result = dbQueryPreparada($sql, [$cuchubalId]);
         $contributions = [];
         while ($row = dbFetchAssoc($result)) {
-            $contributions[] = $row;
+            $contributions[] = new Contribution($row['id'], $row['participant_id'], $row['amount'], $row['date'], $row['cuchubal_id']);
         }
         return $contributions;
     }
 
-    // Actualizar una contribución
-    public function updateContribution($id, $participantId, $amount, $date)
+    // Métodos adicionales según la lógica de negocio
+
+    // Validar contribuciones según el cronograma de pagos
+    public function validateContributionsBySchedule($cuchubalId)
     {
-        $sql = "UPDATE contributions SET participant_id = ?, amount = ?, date = ? WHERE id = ?";
-        return dbQueryPreparada($sql, [$participantId, $amount, $date, $id]);
+        // Implementar lógica para validar las contribuciones en base al cronograma de pagos
+        // Esto puede incluir verificar si las contribuciones se ajustan a las fechas y montos establecidos
     }
 
-    // Soft delete de una contribución
-    public function softDeleteContribution($id)
+    // Actualizar el estado de las contribuciones
+    public function updateContributionStatus($id, $newStatus)
     {
-        $sql = "UPDATE contributions SET active = 0 WHERE id = ?";
-        return dbQueryPreparada($sql, [$id]);
+        $contribution = Contribution::getById($id);
+        if ($contribution) {
+            $contribution->setStatus($newStatus);
+            $contribution->save();
+        }
     }
 
-    // Total de contribuciones
-    public function getTotalContributions()
+    // Listar contribuciones pendientes por cuchubal
+    public function listPendingContributions($cuchubalId)
     {
-        $sql = "SELECT SUM(amount) AS total FROM contributions WHERE active = 1";
-        $result = dbQuery($sql);
+        $sql = "SELECT * FROM contributions WHERE cuchubal_id = ? AND status = 'No pagado'";
+        $result = dbQueryPreparada($sql, [$cuchubalId]);
+        $pendingContributions = [];
+        while ($row = dbFetchAssoc($result)) {
+            $pendingContributions[] = new Contribution($row['id'], $row['participant_id'], $row['amount'], $row['date'], $row['cuchubal_id']);
+        }
+        return $pendingContributions;
+    }
+
+    // Método para calcular el total de contribuciones por cuchubal
+    public function calculateTotalContributions($cuchubalId)
+    {
+        $sql = "SELECT SUM(amount) as total FROM contributions WHERE cuchubal_id = ?";
+        $result = dbQueryPreparada($sql, [$cuchubalId]);
         if ($row = dbFetchAssoc($result)) {
             return $row['total'];
         }
