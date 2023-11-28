08bad1cc16774c7a38527fcecd7188bfc1ad23c7
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-27 19:12:53 -0600

    Add session expiration check in authMiddleware.php

--- backend/auth/authMiddleware.php
+++ backend/auth/authMiddleware.php
@@ -6,4 +6,18 @@ function isAuthenticated()
         echo json_encode(['error' => 'Acceso no autorizado']);
         exit;
     }
+
+    $tiempoMaximoInactividad = 15 * 60; // 15 minutos
+    if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $tiempoMaximoInactividad)) {
+        // Si la sesión ha expirado, limpiar y destruir la sesión
+        session_unset();
+        session_destroy();
+
+        http_response_code(401); // No autorizado
+        echo json_encode(['error' => 'Sesión expirada']);
+        exit;
+    }
+
+    // Actualizar el tiempo de actividad
+    $_SESSION['last_activity'] = time();
 }
