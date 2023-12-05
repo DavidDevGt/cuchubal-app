91d49233dba164773cae4a7485f56a3bd9e5f73c
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-05 17:55:03 -0600

    Add user profile cards and contact information

--- frontend/pages/users.php
+++ frontend/pages/users.php
@@ -17,6 +17,76 @@
     </table>
 </div>
 
+    <!--Container Main start-->
+    
+    <div class="row">
+     
+      <div class="col-sm-4">
+        <div class="card h-100 card-center">
+          <img src="https://www.trecebits.com/wp-content/uploads/2011/09/IMAGEN-DE-PERFIL-FACEBOOK.jpg" class="card-img-top imagen-perfil" alt="...">
+          <div class="card-body card-center">
+            <h5 class="card-title"><b>Juan Luis Guerra</b></h5>
+            <p class="card-text"> <span class="gray">Puesto:</span>  Sistemas <br>
+              <span class="gray">Empresa:</span>  Tubagua S.A. </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="card-title">Información<button class="btn btn-outline-danger float-end">Editar</button></h5>
+            
+            <hr>
+            <p class="card-text">
+              <span class="gray">Nombre:</span> Juan Luis <br>
+              <span class="gray">Apellido:</span> Guerra Paz <br>
+              <span class="gray">Edad:</span> 44 <br>
+              <span class="gray">Puesto:</span> Sistemas <br>
+              <span class="gray">Empresa:</span> Tubagua S.A. <br>
+              <span class="gray">Ciudad:</span> Mixco <br>
+              <span class="gray">Dirección:</span> 15 Calle B Cayala Zona 5 Guatemala <br>
+              <span class="gray">Celular: </span> +502 45488454 <br>
+              <span class="gray">Teléfono:</span> +502 54548154 <br> 
+              <span class="gray">Correo Electrónico:</span> marcapatitogt@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card h-75">
+          <div class="card-body">
+            <h5 class="card-title">Contactos<button class="btn btn-outline-danger float-end">Editar</button></h5>
+            <hr>
+            <p class="card-text"><span class="gray">Esposa: </span> Maria Juana - +502 54656484 <br><br>
+              <span class="gray">Esposa: </span> Maria Juana - +502 54656484 <br><br>
+              <span class="gray">Esposa: </span> Maria Juana - +502 54656484 <br><br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="card-title">Informacion</h5>
+            <p class="card-text"></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card h-100">
+          <div class="card-body">
+            <h5 class="card-title">Informacion</h5>
+            <p class="card-text"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
 <script>
     $(document).ready(function() {
         $.ajax({
