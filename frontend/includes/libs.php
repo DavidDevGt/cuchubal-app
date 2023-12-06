b5f1b1b1b6b08c85fcc6baaf3b84e39fca8e603f
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-06 17:17:59 -0600

    Add SweetAlert2 library for pop-up notifications

--- frontend/includes/libs.php
+++ frontend/includes/libs.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <!-- SCRIPTS -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="js/libs/jquery-3.7.1.js"></script>
 <script src="js/libs/jquery-ui.min.js"></script>
 <script src="js/libs/bootstrap.min.js"></script>
