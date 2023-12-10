20a847acba27c011ea7d011fea4a036fd7c581c8
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-09 18:17:40 -0600

    Add global utility functions to index.php

--- frontend/index.php
+++ frontend/index.php
@@ -14,6 +14,42 @@
     <?php require 'router.php'; ?>
   </div>
   <?php require 'includes/footer/footer.php'; ?>
+  <script>
+    // Funciones globales útiles
+
+    // Validar un monto ingresado si es mayor a cero
+    function validarMontoPositivo(monto) {
+      return parseFloat(monto) > 0;
+    }
+
+    // Función para formatear valores en Quetzales
+    function formatToQuetzales(amount) {
+      const formattedAmount = parseFloat(amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+      return `Q${formattedAmount}`;
+    }
+
+    // Convertir texto a mayúsculas
+    function upperText(texto) {
+      return texto.toUpperCase();
+    }
+
+    // Validar que la fecha de inicio sea menor a la fecha de fin
+    function validarFechasInicioFin(fechaInicio, fechaFin) {
+      const fechaInicioObj = new Date(fechaInicio);
+      const fechaFinObj = new Date(fechaFin);
+
+      return fechaInicioObj < fechaFinObj;
+    }
+
+    // Obtener fecha actual en formato YYYY-MM-DD
+    function obtenerFechaActual() {
+      const fechaActual = new Date();
+      const año = fechaActual.getFullYear();
+      const mes = String(fechaActual.getMonth() + 1).padStart(2, '0');
+      const dia = String(fechaActual.getDate()).padStart(2, '0');
+      return `${año}-${mes}-${dia}`;
+    }
+  </script>
 </body>
 
 </html>
