ba506c35542898b5b55aa71e1408c88bcafc7295
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-09 18:17:57 -0600

    Add dynamic active class to navbar links

--- frontend/includes/navbar/navbar.php
+++ frontend/includes/navbar/navbar.php
@@ -1,3 +1,20 @@
+<?php
+// Obtén la ruta actual desde la URL
+$currentPath = $_SERVER['REQUEST_URI'];
+
+// Define las rutas de las páginas que deseas resaltar como activas
+$dashboardPath = '/cuchubal-app/frontend/';
+$cuchubalesPath = '/cuchubal-app/frontend/cuchubales';
+$pagosPath = '/cuchubal-app/frontend/pagos';
+$participantesPath = '/cuchubal-app/frontend/participantes';
+$usuariosPath = '/cuchubal-app/frontend/usuarios';
+
+// Define una función para verificar si una ruta está activa
+function isActive($currentPath, $targetPath) {
+    return ($currentPath === $targetPath) ? 'active' : '';
+}
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light px-0 py-3">
   <div class="container-xl">
     <!-- Logo -->
@@ -28,12 +45,11 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <!-- Nav -->
       <div class="navbar-nav mx-lg-auto">
-        <a class="nav-item nav-link active" href="#" aria-current="page"
-          >Dashboard</a
-        >
-        <a class="nav-item nav-link" href="#">Cuchubales</a>
-        <a class="nav-item nav-link" href="#">Pagos</a>
-        <a class="nav-item nav-link" href="#">Participantes</a>
+        <a class="nav-item nav-link <?php echo isActive($currentPath, $dashboardPath); ?>" href="<?php echo $dashboardPath; ?>" aria-current="page">Dashboard</a>
+        <a class="nav-item nav-link <?php echo isActive($currentPath, $cuchubalesPath); ?>" href="<?php echo $cuchubalesPath; ?>">Cuchubales</a>
+        <a class="nav-item nav-link <?php echo isActive($currentPath, $pagosPath); ?>" href="<?php echo $pagosPath; ?>">Pagos</a>
+        <a class="nav-item nav-link <?php echo isActive($currentPath, $participantesPath); ?>" href="<?php echo $participantesPath; ?>">Participantes</a>
+        <a class="nav-item nav-link <?php echo isActive($currentPath, $usuariosPath); ?>" href="<?php echo $usuariosPath; ?>">Usuarios</a>
       </div>
       <!-- Right navigation -->
       <div class="navbar-nav ms-lg-4">
