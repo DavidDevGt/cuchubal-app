17566272ebca4d0443dba4dd597fedcd1b3f9c23
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-11 18:03:40 -0600

    Remove initialLocaleCode variable from renderCalendar function

--- frontend/components/calendar/script.js
+++ frontend/components/calendar/script.js
@@ -1,10 +1,8 @@
 function renderCalendar(events) {
-  var initialLocaleCode = 'es';
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
-      locale: initialLocaleCode,
       events: events // Usa los eventos pasados como parámetro
       // Puedes agregar más opciones según sea necesario
   });
